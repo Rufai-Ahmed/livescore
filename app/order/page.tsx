18bce5bcd6ec88c38dfa30860f48a3b769abf985
Rufai-Ahmed <abbeyrufai234@gmail.com>
2024-08-10 15:55:49 +0100

    payment screen added

--- app/order/page.tsx
+++ app/order/page.tsx
@@ -1,35 +1,7 @@
-"use client";
-import TableRow from "../components/TableRow";
-import OrderModal from "../components/OrderModal";
-import { tableData } from "../../public/data/data";
-import { iTableRow } from "../../public/data/types";
+import React from "react";
 
 const Page = () => {
-  return (
-    <div className="overflow-x-auto">
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Food</th>
-            <th>User Name</th>
-            <th>Time</th>
-            <th>Order ID</th>
-            <th>Order Status</th>
-          </tr>
-        </thead>
-
-        <tbody className="border-[5px]">
-          {tableData.map((row: iTableRow, i: number) => (
-            <TableRow i={i} key={i} {...row} />
-          ))}
-        </tbody>
-      </table>
-
-      {tableData.map((_, i) => (
-        <OrderModal i={i} key={i} />
-      ))}
-    </div>
-  );
+  return <div>Page</div>;
 };
 
 export default Page;
--- app/organization/TableRow.tsx
+++ app/organization/TableRow.tsx
@@ -1,50 +1,7 @@
-"use client";
-import { FC, HTMLAttributes } from "react";
-import { CgOrganisation } from "react-icons/cg";
+import React from "react";
 
-const TableRow: FC<iOrganization> = ({
-  _id,
-  address,
-  members,
-  name,
-  email,
-  accessCode,
-  phone,
-  ...props
-}) => {
-  const handleClick = () => {
-    (
-      document.getElementById(`orgModal${_id}`) as HTMLDialogElement
-    )?.showModal()!;
-  };
-
-  return (
-    <>
-      <tr {...props} className="border-y border-2 cursor-pointer">
-        <td onClick={handleClick} className="border-y-[5px] ">
-          <div className="flex items-center gap-3">
-            <div className="avatar">
-              <div className="mask mask-squircle">
-                <CgOrganisation color="#a72036" size={30} />
-              </div>
-            </div>
-            <div>
-              <div className="font-bold">{name}</div>
-              <div className="text-sm opacity-50">ID: {_id}</div>
-            </div>
-          </div>
-        </td>
-        <td className="font-medium border-y-[5px] ">{address}</td>
-        <td className="font-medium border-y-[5px] ">{email}</td>
-
-        <td className="border-y-[5px] ">{phone}</td>
-
-        <td className="border-y-[5px]  ">
-          {members?.length ? members?.length : 0}
-        </td>
-      </tr>
-    </>
-  );
+const TableRow = () => {
+  return <div>TableRow</div>;
 };
 
 export default TableRow;
