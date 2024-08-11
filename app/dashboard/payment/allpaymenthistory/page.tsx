94c6ff27efe0e89e3b02a27b072bf4b1af91c550
Rufai Ahmed <abbeyrufai234@gmail.com>
2024-08-11 13:15:27 +0100

    Update page.tsx

--- app/dashboard/payment/allpaymenthistory/page.tsx
+++ app/dashboard/payment/allpaymenthistory/page.tsx
@@ -3,10 +3,10 @@ import { useGetPaymentsQuery } from "@/public/utils/authApi";
 import React from "react";
 
 const AllPaymentsHistoryPage = () => {
-  const { data: payments = [], isLoading } = useGetPaymentsQuery();
+  const { data: payments = [], isLoading } :any = useGetPaymentsQuery();
 
   // Group payments by date
-  const groupedPayments = payments?.reduce((acc: any, payment: any) => {
+  const groupedPayments:any = payments?.reduce((acc: any, payment: any) => {
     const date = new Date(payment?.session).toDateString();
     if (!acc[date]) {
       acc[date] = [];
@@ -21,7 +21,7 @@ const AllPaymentsHistoryPage = () => {
     <div>
       <h1>All Payments History</h1>
       <div className="space-y-4">
-        {Object.entries(groupedPayments).map(([date, payments]) => (
+        {Object?.entries(groupedPayments)?.map(([date, payments]) => (
           <div key={date} className="border border-gray-300 p-4 rounded-lg">
             <h2 className="text-xl font-bold">{date}</h2>
             <ul className="mt-2">
